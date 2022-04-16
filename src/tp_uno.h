e279ef5d9092aa5486f390d9e0da470e276bfead
GermanMolinari <germang.molinari@gmail.com>
2022-04-16 17:03:35 -0300

    se agrego documentacion de funciones

--- src/tp_uno.h
+++ src/tp_uno.h
@@ -8,12 +8,77 @@
 #ifndef TP_UNO_H_
 #define TP_UNO_H_
 
+/** \brief Imprime el menu y devuelve la opcion ingresada
+ *
+ *  \param float* puntero a kilometros
+ *  \param float* mensaje mensaje previo a pedir el dato
+ *  \param float* mensajeError muestra el mensaje si se ingresa mal el dato
+ *  
+ *  \return retorna la opcion ingresada x el usuario
+ *
+ */
 int menu(float* kilometros, float* precioAerolineas, float* precioLatam);
+
+
+/** \brief calcula el descuento, debe ser ingresado con 0.cantidaDeDescuento
+ *
+ *  \param float precio
+ *  \param float descuento a aplicar
+ *  
+ *  \return retorna el precio con el descuento aplicado
+ *
+ */
+
 float calcularDescuento (float precio, float descuento);
+
+/** \brief calcula el interes, debe ser ingresado con 0.cantidaDeInteres
+ *
+ *  \param float precio
+ *  \param float interes a aplicar
+ *  
+ *  \return retorna el precio con el interes aplicado
+ *
+ */
 float calcularInteres (float precio, float interes);
+
+/** \brief divide dos flotantes
+ *
+ *  \param float divisor
+ *  \param float dividendo
+ *  \param float* escribe el resultado en la direccion de memoria
+ *  \return retorna el 0 o 1 dependiendo si el divisor es != 0
+ *
+ */
+
 int dividirFlotantes (float divisor, float dividendo, float* pResultado);
+
+/** \brief Multiplica dos flotantes
+ *
+ *  \param float operando a
+ *  \param float operando b
+ *  
+ *  \return retorna el resultado de la multiplicacion
+ *
+ */
 float multiplicarFlotantes(float a, float b);
+
+/** \brief resta dos flotantes y devuelve el resultado siempre positivo
+
+ *  \param float operando a
+ *  \param float operando b
+ *  
+ *  \return retorna el resultado positivo
+ *
+ */
 float restarFlotantes (float a, float b);
+
+/** \brief Realiza todos los calculos correspondientes y los muestra en pantalla
+ *
+ *  \param float Km
+ *  \param float precio uno
+ *  \param precio dos
+ *
+ */
 void cargarForzadamente (float kmIngresados, float precioAerolineas, float precioLatam);
 
 #endif /* TP_UNO_H_ */
--- src/validaciones.h
+++ src/validaciones.h
@@ -8,7 +8,32 @@
 #ifndef VALIDACIONES_H_
 #define VALIDACIONES_H_
 
+/** \brief Pide un entero y lo devuelve validado en un rango, muestra un mensaje antes de pedirlo
+ *
+ *  \param int * pNumeroIngresado donde escribe el entero si es valido
+ *  \param char* mensaje mensaje previo a pedir el dato
+ *  \param char* mensajeError muestra el mensaje si se ingresa mal el dato
+ *  \param int maximo el maximo aceptado para validar
+ *  \param int minimo el minimo aceptado para validar
+ *  \param int maximoDeReintentos el numero maximo de reintentos para ingresar un entero valido
+ *  \return retorna si 1 si pudo hacerlo con exito
+ *
+ */
+
 int getInt(int * pNumeroIngresado,char * mensaje,char * mensajeError,int maximo, int minimo, int maximoDeReintentos);
+
+
+/** \brief Pide un entero y lo devuelve validado en un rango, muestra un mensaje antes de pedirlo
+ *
+ *  \param float * pNumeroIngresado donde escribe el entero si es valido
+ *  \param char* mensaje mensaje previo a pedir el dato
+ *  \param char* mensajeError muestra el mensaje si se ingresa mal el dato
+ *  \param float maximo el maximo aceptado para validar
+ *  \param float minimo el minimo aceptado para validar
+ *  \param float maximoDeReintentos el numero maximo de reintentos para ingresar un entero valido
+ *  \return retorna si 1 si pudo hacerlo con exito
+ *
+ */
 int getFloat(float * pNumeroIngresado,char * mensaje,char * mensajeError, float maximo, float minimo, int maximoDeReintentos);
 
 
